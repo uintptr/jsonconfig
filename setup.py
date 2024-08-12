24274ab721bc5de17bc213c7370055ec28f11ba0
fsck <inaforloop@gmail.com>
2024-08-12 18:51:46 -0400

    fix name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='py-jsonconfig'
+    name='py_jsonconfig'
     version='0.1',
     packages=find_packages(),
     install_requires=[],
