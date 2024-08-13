4b9d2dbc6fd52c925c3cc192190cfe6eddb72e40
fsck <inaforloop@gmail.com>
2024-08-13 18:54:45 -0400

    update stub

--- jsonconfig/__init__.pyi
+++ jsonconfig/__init__.pyi
@@ -1,5 +1,8 @@
 
+from typing import Any
+
 class JSONConfig:
+    def __init__(self, file_path: str) -> None:
     def get(self, path: str, default: Any | None = None) -> Any:
     def get_int(self, path: str, default: int | None = None) -> int:
     def get_str(self, path: str, default: str | None = None) -> str:
